adc26b7a352d5a77f5283d3729baaadc1a069974
Qunnnn <vantranquan123@gmail.com>
2023-09-07 23:31:25 +0700

    inprocess(Blocs) Auth Bloc flow

--- fb_auth_bloc/lib/main.dart
+++ fb_auth_bloc/lib/main.dart
@@ -1,18 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fb_auth_bloc/blocs/auth/auth_bloc.dart';
+import 'package:fb_auth_bloc/blocs/signin/signin_cubit.dart';
+import 'package:fb_auth_bloc/pages/pages.dart';
+import 'package:fb_auth_bloc/repositories/auth_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthRepository>(
+          create: (context) => AuthRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+            firebaseAuth: FirebaseAuth.instance,
+          ),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+            create: (context) => AuthBloc(
+              authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+          BlocProvider<SigninCubit>(
+            create: (context) => SigninCubit(
+              authRepository: context.read<AuthRepository>(),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Firebase Auth',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const SplashPage(),
+          routes: {
+            SignupPage.routeName: (context) => const SignupPage(),
+            SigninPage.routeName: (context) => const SigninPage(),
+            HomePage.routeName: (context) => const HomePage(),
+          },
+        ),
       ),
     );
   }
---
+++ fb_auth_bloc/lib/pages/signup_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  static const String routeName = '/signup';
+  const SignupPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
